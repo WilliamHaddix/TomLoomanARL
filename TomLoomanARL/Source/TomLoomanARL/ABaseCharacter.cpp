5a073b925b5e53b0acf0290eaa2f5a659f41e2d0
willhaddix <will@battleshibastudios.com>
2025-07-20 15:50:51 -0500

    Updated gitignore

---
+++ TomLoomanARL/Source/TomLoomanARL/ABaseCharacter.cpp
@@ -0,0 +1,5 @@
+#include "ABaseCharacter.h"
+
+ABaseCharacter::ABaseCharacter() {
+  
+}
---
+++ TomLoomanARL/Source/TomLoomanARL/ABaseCharacter.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "GameFramework/Character.h"
+#include  "ABaseCharacter.generated.h"
+
+UCLASS()
+class ABaseCharacter : public ACharacter
+{
+    GENERATED_BODY()
+  
+public:
+    ABaseCharacter();
+};
