ee85f8b2cc99c8cb82d21108ffcf6a2427699ef7
willhaddix <will@battleshibastudios.com>
2025-07-20 17:03:22 -0500

    Lecture 1
    Added Movement and Camera
    Tom Looman teaches it without Input Mapping since it's UE4 based

--- TomLoomanARL/Source/TomLoomanARL/ABaseCharacter.cpp
+++ TomLoomanARL/Source/TomLoomanARL/ABaseCharacter.cpp
@@ -1,5 +1,88 @@
 #include "ABaseCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+DEFINE_LOG_CATEGORY(LogBaseCharacter);
+
 ABaseCharacter::ABaseCharacter() {
+    PrimaryActorTick.bCanEverTick = true;
   
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+    SpringArm->SetupAttachment(RootComponent);
+    SpringArm->TargetArmLength = 400.f;
+  
+    CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    CameraComp->SetupAttachment(SpringArm);
+}
+
+//////////////////////////////////////////////////////////////////////////// Input Setup
+
+void ABaseCharacter::NotifyControllerChanged()
+{
+    Super::NotifyControllerChanged();
+	
+    // Add Input Mapping Context
+    if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(DefaultMappingContext, 0);
+        }
+    }
+}
+
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    // Set up action bindings
+    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        // Moving
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Move);
+        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Look);
+    }
+    else
+    {
+        UE_LOG(LogBaseCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////// Input Functions
+
+void ABaseCharacter::Move(const FInputActionValue& Value)
+{
+    // input is a Vector2D
+    FVector2D MovementVector = Value.Get<FVector2D>();
+
+    if (Controller != nullptr)
+    {
+        // find out which way is forward
+        const FRotator Rotation = Controller->GetControlRotation();
+        const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+        // get forward vector
+        const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+        // get right vector
+        const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+        // add movement
+        AddMovementInput(ForwardDirection, MovementVector.Y);
+        AddMovementInput(RightDirection, MovementVector.X);
+    }
+}
+
+void ABaseCharacter::Look(const FInputActionValue& Value)
+{
+    // input is a Vector2D
+    FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+    if (Controller != nullptr)
+    {
+        // add yaw and pitch input to controller
+        AddControllerYawInput(LookAxisVector.X);
+        AddControllerPitchInput(LookAxisVector.Y);
+    }
 }
--- TomLoomanARL/Source/TomLoomanARL/ABaseCharacter.h
+++ TomLoomanARL/Source/TomLoomanARL/ABaseCharacter.h
@@ -1,13 +1,47 @@
 #pragma once
 
 #include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
 #include  "ABaseCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogBaseCharacter, Log, All);
 
 UCLASS()
 class ABaseCharacter : public ACharacter
 {
     GENERATED_BODY()
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UCameraComponent>  CameraComp;
+    
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USpringArmComponent> SpringArm;
+    
+    /** Mapping Context */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+    /** Move Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputAction> MoveAction;
+
+    /** Look Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputAction> LookAction;
   
 public:
     ABaseCharacter();
+
+protected:
+    void Move(const FInputActionValue& Value);
+    void Look(const FInputActionValue&Value);
+    
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+    virtual void NotifyControllerChanged() override;
 };
